0f453121b494832fad96db37059febb2366168e4
Winston maben <72695682+Winstonmn99@users.noreply.github.com>
2024-03-22 16:09:16 +0530

    Update leetCode_100.py

--- Python/leetCode_100.py
+++ Python/leetCode_100.py
@@ -46,3 +46,42 @@ def target(num,k):
 nums = [2,7,11,15]
 k=9
 print(target(nums,k))
+
+
+#You are given  words. Some words may repeat. For each word, output its number of occurrences. 
+#The output order should correspond with the input order of appearance of the word. See the sample input/output for clarification.
+
+#Input
+#4
+#bcdef
+#abcdefg
+#bcde
+#bcdef
+
+#output:
+#3
+#2 1 1
+
+def word_format(word):
+    word_input = []
+    visited = []
+    counted = []
+    for i in range(word):
+        lists = str(input())
+        word_input.append(lists)
+    for n in range(len(word_input)):
+        visited.append(False)
+    for j in range(len(word_input)):
+        if visited[j]:
+            continue
+        count = 0
+        for k in range(len(word_input)):
+            if word_input[j] == word_input[k]:
+                count = count + 1
+                visited[k] = True
+        counted.append(count)
+    print(len(counted))
+    print(*counted, sep=" ")
+
+words = int(input())
+word_format(words)
